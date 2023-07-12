fd61e2d94d01780dc3b88bf597d91439db7320db
Sofya Tuymedova <stuymedova@gmail.com>
2023-07-12 11:56:06 +0300

    Test: adding vibration

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import StateMachine from './StateMachine.js';
 import Indicator from './Indicator.js';
 
 export default class App {
-  inhaleDuration = 4;
-  holdDuration = 7;
-  exhaleDuration = 8;
+  inhaleDuration = 4; // in sec
+  holdDuration = 7; // in sec
+  exhaleDuration = 8; // in sec
+
+  canVibrate = Boolean(navigator.vibrate);
+  vibrationDuration = 400; // in msec
 
   constructor() {
     const states = {
@@ -22,21 +25,30 @@ export default class App {
   animate(indicator) {
     const currentState = this.stateMachine.currentState;
     let timeoutTime = 0;
-    
+
     if (currentState === 'inhaling') {
+      if (this.canVibrate) {
+        navigator.vibrate(this.vibrationDuration);
+      }
       indicator.expand(this.inhaleDuration);
       timeoutTime = this.inhaleDuration;
     }
-    
+
     if (currentState === 'holding') {
+      if (this.canVibrate) {
+        navigator.vibrate(this.vibrationDuration);
+      }
       timeoutTime = this.holdDuration;
     }
-    
+
     if (currentState === 'exhaling') {
+      if (this.canVibrate) {
+        navigator.vibrate([this.vibrationDuration, this.vibrationDuration]);
+      }
       indicator.contract(this.exhaleDuration);
       timeoutTime = this.exhaleDuration;
     }
-    
+
     // Delay further execution for some time
     setTimeout(() => {
       this.stateMachine.enterNextState();
